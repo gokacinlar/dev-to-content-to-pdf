d30ce666abefbb7a889dbde186cc4259ad50165b
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2024-07-20 02:34:55 +0300

    fix: refactor pdf & text savings, made readable

--- src/content.js
+++ src/content.js
@@ -1,9 +1,8 @@
 // retrieve the document's title
 chrome.storage.local.get("documentTitle", (title) => {
     if (chrome.runtime.lastError) {
-        console.error("Error retrieving captured content:", chrome.runtime.lastError);
+        console.error("Error retrieving document title:", chrome.runtime.lastError);
     } else {
-        // assign article name to later recall it using template literal
         const articleName = title.documentTitle;
 
         // retrieve the actual content of the DOM
@@ -11,18 +10,135 @@ chrome.storage.local.get("documentTitle", (title) => {
             if (chrome.runtime.lastError) {
                 console.error("Error retrieving captured content:", chrome.runtime.lastError);
             } else {
+                const documentContent = data.capturedContent;
+
                 /**
-                 * Save the content as PDF
+                 * Generate PDF from the article content
                  */
 
                 const saveAsPdfButton = document.getElementById("saveAsPdf");
                 const spinner = saveAsPdfButton.querySelector(".spinner-border");
                 const buttonText = saveAsPdfButton.querySelector(".button-text");
 
-                saveAsPdfButton.addEventListener("click", generateArticlePDF);
+                saveAsPdfButton.addEventListener("click", () => {
+                    initialButtonStage();
+                    setTimeout(() => {
+                        const articleSource = document.getElementById("clonedContentDiv");
+                        stylingArticle(articleSource);
+                        if (articleSource) {
+                            const opt = {
+                                margin: [0, 0, 0, 0],
+                                filename: `${articleName}.pdf`,
+                                image: { type: 'jpeg', quality: 1 },
+                                html2canvas: { scale: 2, letterRendering: true, allowTaint: false, useCORS: true },
+                                pagebreak: { mode: ['css', 'avoid-all', 'legacy'] },
+                                jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+                                enableLinks: true
+                            };
+                            html2pdf().set(opt).from(articleSource).save().then(() => {
+                                revertArticle(articleSource);
+                            });
+                        } else {
+                            alert("Article source could not be found!");
+                        }
+                        spinnerStage();
+                    }, 250);
+                });
+
+                /**
+                 * Save the content as HTML
+                 */
+                const saveAsHtmlButton = document.getElementById("saveAsHtml");
+                saveAsHtmlButton.addEventListener("click", () => {
+                    const tempContainer = document.createElement("div");
+                    tempContainer.innerHTML = documentContent;
+
+                    // remove elements that is unnecessary in here with foreach function
+                    const idsToRemove = [
+                        "#comments",
+                        "#hide-comments-modal",
+                        "#hide-comments-modal__form"
+                    ];
+
+                    idsToRemove.forEach(id => removeUnnecessaryElementsFromContent(id, tempContainer));
+
+                    const htmlBlob = new Blob([tempContainer.innerHTML], { type: "text/html" });
+                    downloadHtmlBlob(htmlBlob, `${articleName}.html`);
+                });
+
+                /**
+                 * Save the content as plain Text with options of html and without html
+                 */
+
+                const saveAsTextButton = document.getElementById("saveAsText");
+                const saveAsTextWithHtmlButton = document.getElementById("saveAsTextWithHtml");
+
+                saveAsTextButton.addEventListener("click", () => {
+                    const cleanedText = formatTextWithoutHtml(documentContent);
+                    const cleanedTextBlob = new Blob([cleanedText], { type: "text/plain" });
+                    downloadTextBlob(cleanedTextBlob, `${articleName}.txt`);
+                });
+
+                saveAsTextWithHtmlButton.addEventListener("click", () => {
+                    const cleanedTextWithHtml = formatTextWithHtml(documentContent);
+                    const cleanedTextWithHtmlBlob = new Blob([cleanedTextWithHtml], { type: "text/plain" });
+                    downloadTextBlob(cleanedTextWithHtmlBlob, `${articleName}.txt`);
+                });
+
+                /**
+                 * Helper Functions
+                 */
+
+                // function to remove elements from the captured content while saving
+                function removeUnnecessaryElementsFromContent(selectorOrElement, container) {
+                    if (typeof selectorOrElement === "string") {
+                        container.querySelectorAll(selectorOrElement).forEach(elem => elem.remove());
+                    } else if (selectorOrElement instanceof Element) {
+                        selectorOrElement.remove();
+                    } else {
+                        console.error("Invalid argument: Must be a CSS selector string or a DOM element");
+                    }
+                }
+
+                function downloadHtmlBlob(blob, name = "file.html") {
+                    const blobUrl = URL.createObjectURL(blob);
+                    const link = document.createElement("a");
+                    link.style.display = "none";
+                    link.href = blobUrl;
+                    link.download = name;
+                    document.body.appendChild(link);
+                    link.click();
+                    window.URL.revokeObjectURL(blobUrl);
+                }
+
+                function downloadTextBlob(blob, name = "file.txt") {
+                    const blobUrl = URL.createObjectURL(blob);
+                    const link = document.createElement("a");
+                    link.style.display = "none";
+                    link.href = blobUrl;
+                    link.download = name;
+                    document.body.appendChild(link);
+                    link.click();
+                    window.URL.revokeObjectURL(blobUrl);
+                }
+
+                function formatTextWithoutHtml(input) {
+                    let originalText = input.replace(/<\/?[^>]+(>|$)/g, " ");
+                    return originalText.replace(/^\h\s*[\r\n]/gm, " ");
+                }
+
+                function formatTextWithHtml(input) {
+                    let cleanedText = input;
+                    let divWrapper = document.createElement("div");
+                    divWrapper.innerHTML = cleanedText.replace(/(<([^>]+)>)/ig, "");;
+                    return cleanedText;
+                }
+
+                /**
+                 * Styling functions
+                 */
 
                 function initialButtonStage() {
-                    // show the spinner and hide the text
                     spinner.classList.remove("d-none");
                     buttonText.classList.add("d-none");
                 }
@@ -33,138 +149,13 @@ chrome.storage.local.get("documentTitle", (title) => {
                 }
 
                 function stylingArticle(elem) {
-                    const changedWidth = elem;
-                    changedWidth.setAttribute("style", "width: 98%;");
+                    elem.setAttribute("style", "width: 98%;");
                 }
 
                 function revertArticle(elem) {
-                    const revertedWidth = elem;
-                    revertedWidth.removeAttribute("style", "width: 98%;");
-                    revertedWidth.setAttribute("class", "col-12 col-sm-12 col-md-8 rounded");
+                    elem.removeAttribute("style", "width: 98%;");
+                    elem.setAttribute("class", "col-12 col-sm-12 col-md-8 rounded");
                 }
-
-                function generateArticlePDF() {
-                    initialButtonStage();
-                    setTimeout(() => {
-                        const articleSource = document.getElementById("clonedContentDiv");
-                        stylingArticle(articleSource);
-                        if (articleSource) {
-                            // html2pdf options
-                            var opt = {
-                                margin: 0,
-                                filename: `${articleName}.pdf`,
-                                image: { type: 'jpeg', quality: 5 },
-                                html2canvas: { scale: 4, letterRendering: 1, allowTaint: true },
-                                pagebreak: { mode: 'avoid-all' },
-                                jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
-                                enableLinks: true
-                            };
-                            // save with new promise based api
-                            html2pdf().set(opt).from(articleSource).save().then(() => {
-                                revertArticle(articleSource);
-                            });
-                        } else {
-                            initialButtonStage();
-                            return alert("Article source could not be found!");
-                        }
-                        spinnerStage();
-                    }, 250);
-                }
-
-                /**
-                 * Save the content as HTML
-                 */
-
-                const saveAsHtmlButton = document.getElementById("saveAsHtml");
-                saveAsHtmlButton.addEventListener("click", () => {
-                    downloadHtmlBlob(htmlBlob, `${articleName}.html`);
-                });
-
-                // define the content
-                const documentContent = data.capturedContent;
-
-                // define the function to save HTML DOM as plain text via blob
-                function downloadHtmlBlob(blob, name = "file.html") {
-                    const blobUrl = URL.createObjectURL(blob);
-                    const link = document.createElement("a");
-
-                    link.style.display = "none";
-                    link.href = blobUrl;
-                    link.download = name;
-
-                    document.body.appendChild(link);
-                    link.click();
-
-                    window.URL.revokeObjectURL(blobUrl);
-                }
-
-                const htmlBlob = new Blob([documentContent], { type: "text/html" }); // specify the mime type
-                /**
-                 * Save the content as plain Text
-                 */
-
-                // function to format the output of the content on TXT file
-                function formatTextWithoutHtml(input) {
-                    let originalText = input.replace(/<\/?[^>]+(>|$)/g, " ");
-                    let cleanedText = originalText.replace(/^\h\s*[\r\n]/gm, " ");
-                    let divWrapper = document.createElement("div");
-                    divWrapper.innerHTML = cleanedText;
-                    return cleanedText;
-                }
-
-                // function to format the output of the content on TXT file
-                // including HTML tags
-                function formatTextWithHtml(input) {
-                    let cleanedText = input;
-                    let divWrapper = document.createElement("div");
-                    divWrapper.innerHTML = cleanedText.replace(/(<([^>]+)>)/ig, "");;
-                    return cleanedText;
-                }
-
-                const saveAsTextButton = document.getElementById("saveAsText");
-                const saveAsTextWithHtmlButton = document.getElementById("saveAsTextWithHtml");
-
-                function downloadTextBlob(blob, name = "file.txt") {
-                    const blobUrl = URL.createObjectURL(blob);
-                    const link = document.createElement("a");
-
-                    link.style.display = "none";
-                    link.href = blobUrl;
-                    link.download = name;
-
-                    document.body.appendChild(link);
-                    link.click();
-
-                    window.URL.revokeObjectURL(blobUrl);
-                }
-
-                // use FileReader object to read the blob, format it back in plain txt
-                // and then pass onto the saveAsTextButton addEventListener
-                const reader = new FileReader();
-                reader.onload = function () {
-                    const text = reader.result;
-                    const cleanedText = formatTextWithoutHtml(text);
-                    const cleanedTextBlob = new Blob([cleanedText], { type: "text/plain" });
-
-                    saveAsTextButton.addEventListener("click", () => {
-                        downloadTextBlob(cleanedTextBlob, `${articleName}.txt`);
-                    });
-                }
-
-                reader.readAsText(new Blob([documentContent], { type: "text/plain" }));
-
-                const readerForHtmlText = new FileReader();
-                readerForHtmlText.onload = function () {
-                    const text = readerForHtmlText.result;
-                    const cleanedTextWithHtml = formatTextWithHtml(text);
-                    const cleanedTextWithHtmlBlob = new Blob([cleanedTextWithHtml], { type: "text/plain" });
-
-                    saveAsTextWithHtmlButton.addEventListener("click", () => {
-                        downloadTextBlob(cleanedTextWithHtmlBlob, `${articleName}.txt`)
-                    })
-                }
-
-                readerForHtmlText.readAsText(new Blob([documentContent], { type: "text/plain" }));
             }
         });
     }
